7d9b44c82df8b886464175b6f6a02fb1cea87e5b
Andrew Whitaker <acw123103478@gmail.com>
2021-11-29 15:54:06 -0500

    Can now subscribe + publish to a list of topics. When a message is recieved from a subscription, we record the topic in the successfultopics set.

--- MQTT.py
+++ MQTT.py
@@ -3,11 +3,16 @@
 import paho.mqtt.client as paho
 
 
+connected = False
+successfultopics = set([])
+
+
 def create_client(username, password):
     print("Creating client")
     client = paho.Client()
     client.on_connect = on_connect
     client.on_message = on_message
+    client.on_publish = on_publish
     client.username_pw_set(username, password)
 
     return client
@@ -20,12 +25,23 @@ def client_connect(client, host, port):
 
 def on_message(client, userdata, msg):
     print("In On_message!")
-    print("Topic: "+msg.topic+" Message: "+str(msg.payload))
+    print("Topic: ", msg.topic, " Message: ", str(msg.payload))
+    successfultopics.add(msg.topic)
+    print("List of successful topics is now: ", successfultopics)
 
 
-def on_connect(client,userdata,flags,rc):
-    print("In On_connect!")
-    payload = "test from ACL tool!"
-    client.subscribe("test/message")
-    client.publish("test/message", payload, qos=0, retain=False)
+def on_connect(client, userdata, flags, rc):
+    # print("In On_connect!")
+    global connected
+    if rc == 0:
+        print("Successfully connected")
+        connected = True
+    else:
+        print("Authorization error, wrong password")
+
+
+
+def on_publish(client, userdata, mid):
+    print("Published message with ID: ", mid)
+
 
--- main.py
+++ main.py
@@ -1,17 +1,29 @@
 # Main file
 import MQTT
+import time
+
+topiclist = ["test/message1", "test/message2", "blah/message"]
+
 
 def main():
     print("Hello World!")
 
+    # process user input here
+
     # will need to set user/pass manually for now
     client = MQTT.create_client("user", "password")
+
 
     # will need to change hostname manually for now
     MQTT.client_connect(client, "mqttserver", 1883)
 
-    client.loop_forever()
+    for topic in topiclist:
+        message = "This message is from topic: " + topic
+        client.subscribe(topic)
+        client.publish(topic, message, qos=0, retain=False)
 
+    client.loop_forever()
+    client.disconnect()
 
 
 if __name__ == "__main__":
