40a5dc58c044ce34f1b1098067d75ccc1f10633b
Andrew Whitaker <acw123103478@gmail.com>
2021-11-28 21:17:40 -0500

    Create basic framework, able to publish a message to MQTT server.

--- MQTT.py
+++ MQTT.py
@@ -1,2 +1,29 @@
 # MQTT functions go here (publish, subscribe, etc.)
 
+import paho.mqtt.client as paho
+
+
+def create_client(username, password):
+    print("Creating client")
+    client = paho.Client()
+    client.on_connect = on_connect
+    client.on_message = on_message
+    client.username_pw_set(username, password)
+
+    return client
+
+
+def client_connect(client, host, port):
+    print("Connecting to host")
+    client.connect(host, port)
+
+
+def on_message():
+    print("In On_message!")
+
+
+def on_connect(client,userdata,flags,rc):
+    print("In On_connect!")
+    payload = "test from ACL tool!"
+    client.publish("test/message", payload, qos=0, retain= False)
+
--- main.py
+++ main.py
@@ -1,1 +1,19 @@
 # Main file
+import MQTT
+
+def main():
+    print("Hello World!")
+
+    # will need to set user/pass manually for now
+    client = MQTT.create_client("user", "password")
+
+    # will need to change hostname manually for now
+    MQTT.client_connect(client, "mqttserver", 1883)
+
+    client.loop_forever()
+
+
+
+if __name__ == "__main__":
+    main()
+
